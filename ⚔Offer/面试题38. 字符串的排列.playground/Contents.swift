14b19542893d943e0a2ee4f9b0d831b65e8eedb1
iAronTalk <xinqi_aaron@126.com>
2020-03-25 18:00:36 +0800

    add string permutatin to sword to offer ii

--- ⚔Offer/面试题38. 字符串的排列.playground/Contents.swift
+++ ⚔Offer/面试题38. 字符串的排列.playground/Contents.swift
@@ -12,7 +12,6 @@ import UIKit
  */
 
 //这种做法与offer书给出来的不一样
-//后面补齐offer书的办法。
 class Solution {
     var res = [String]()
     func permutation(_ s: String) -> [String] {
@@ -21,7 +20,7 @@ class Solution {
         }
         var visited = [Bool].init(repeating: false, count: s.count)
         def(s, "", &visited)
-        return Array(Set(res))
+        return Array(Set(res)) //注意判重
     }
     
     func def(_ s: String, _ letters: String, _ visited: inout [Bool]) -> Void {
@@ -36,6 +35,35 @@ class Solution {
             visited[index] = true
             def(s, letters + String(char), &visited)
             visited[index] = false
+        }
+    }
+}
+
+//offer book的写法，速度太慢，无法通过。
+class Solution2 {
+    var res = [String]()
+    func permutation(_ s: String) -> [String] {
+        if s.count == 0 {
+            return []
+        }
+        var characters = Array(s)
+        def(&characters, 0)
+        return Array(Set(res)) //注意判重
+    }
+    func def(_ s: inout [Character], _ pos: Int) {
+        if pos == s.count {
+            res.append(String(s))
+            return
+        }
+        var visited = [Bool].init(repeating: false, count: s.count)
+        for index in 0..<s.count {
+            if visited[index] {
+                continue
+            }
+            visited[index] = true
+            s.swapAt(index, pos)
+            def(&s, pos + 1)
+            s.swapAt(index, pos)
         }
     }
 }
