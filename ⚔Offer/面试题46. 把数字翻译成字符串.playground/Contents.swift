409f30b112d18848a46a65b332e79fd2ffebe173
iAronTalk <xinqi_aaron@126.com>
2020-03-31 14:42:32 +0800

    add [TranslateNumbersToStrings]to sword to offer

---
+++ ⚔Offer/面试题46. 把数字翻译成字符串.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+/*
+ 给定一个数字，我们按照如下规则把它翻译为字符串：0 翻译成 “a” ，1 翻译成 “b”，……，11 翻译成 “l”，……，25 翻译成 “z”。一个数字可能有多个翻译。请编程实现一个函数，用来计算一个数字有多少种不同的翻译方法。
+
+ 示例 1:
+ 输入: 12258
+ 输出: 5
+ 解释: 12258有5种不同的翻译，分别是"bccfi", "bwfi", "bczi", "mcfi"和"mzi"
+  
+ 提示：
+ 0 <= num < 231
+ */
+
+//自己分析+参考
+class Solution {
+    func translateNum(_ num: Int) -> Int {
+        if num < 10 {return 1}
+        let remainder = num % 100
+        if remainder <= 9 || remainder >= 26 {
+            return translateNum(num / 10)
+        } else {
+            return translateNum(num / 10) + translateNum(num / 100)
+        }
+    }
+}
